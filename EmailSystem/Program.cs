5877ee3d91c0db2d56a966a1a321e4d6f66e2591
Mateja <matejajovic2002@gmail.com>
2025-02-12 18:58:23 +0100

    Added specific project for every data base provider

--- EmailSystem/Program.cs
+++ EmailSystem/Program.cs
@@ -1,12 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<Context>();
 builder.Services.AddControllers();
+
+
+var databaseProvider = builder.Configuration["DatabaseProvider"];
+var connectionString = builder.Configuration.GetConnectionString(databaseProvider);
+builder.Services.AddDbContext<Context>(options =>
+{
+    switch (databaseProvider)
+    {
+        case "SqlServer":
+            options.UseSqlServer(connectionString, sqlOptions =>
+                sqlOptions.MigrationsAssembly("EmailSystem.Migrations.SqlServer"));
+            break;
+        case "PostgreSQL":
+            options.UseNpgsql(connectionString, npgsqlOptions =>
+                npgsqlOptions.MigrationsAssembly("EmailSystem.Migrations.PostgreSQL"));
+            break;
+        case "MySQL":
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), mySqlOptions =>
+                mySqlOptions.MigrationsAssembly("EmailSystem.Migrations.MySQL"));
+            break;
+        default:
+            throw new Exception("Nepodržan tip baze: " + databaseProvider);
+    }
+});
 
 var app = builder.Build();
 
