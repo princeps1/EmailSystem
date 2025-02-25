e9b3d6baa8fc488217cf87265e99ede423ad1762
Mateja <matejajovic2002@gmail.com>
2025-02-25 13:27:56 +0100

    .

--- EmailSystem.Contracts/IEmailService.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace EmailSystem.Contracts;
-public interface IEmailService
-{
-    public Task PosaljiEmailAsync(string subject, string toEmail, string username, string message);
-}
--- EmailSystem.Contracts/IFileService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EmailSystem.Contracts;
-public interface IFileService
-{
-    string ProcessFile(Stream content);
-    List<KeyValuePair<string, int>> ProcessFileAsString(Stream fileStream,string returnOnlyThisLevel);
-}
